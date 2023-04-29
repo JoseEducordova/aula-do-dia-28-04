405a96b58ee89a0dbb19ecea7b95989bdad0f352
JoseEducordova <130517843+JoseEducordova@users.noreply.github.com>
2023-04-28 21:03:49 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+#aula-dia-27-04
+#aula-dia-27-04
+#aula-dia-27-04
---
+++ index.js
@@ -0,0 +1,55 @@
+/*const nome = prompt("nome?")
+console.log (`O ${nome} "é turista"`);*/
+
+/*const nome = prompt ("Qual seu nome? ")
+const corFavorita = prompt("Qual sua cor favorita? ")
+
+console.log(`Seu nome é ${nome}  e sua cor favorita é  ${corFavorita}`);
+console.log("Seu nome é: ", nome , "e sua cor favorita é: " , corFavorita );*/
+
+/*const nome = prompt ("digite seu nome: ")
+console.log(nome.toLowerCase())
+console.log(nome.toUpperCase())*/
+
+/*const frase= prompt ("Digite uma frase: ");
+
+console.log(frase.toUpperCase());
+console.log(frase.replaceAll("o", "i"));
+console.log(frase.length);
+
+console.log(frase.replaceAll("o", "i").toUpperCase(), frase.length);*/
+
+/*const numeros = [1,2,3,4,5];
+console.log(numeros[0])*/
+
+/*const raçaDeCachorro = ["PastorAlemão", "Husky", "Rottwailler", "Caramelo", "Shitsu"]
+const raçaDeCachorro1 = Number(prompt ("Digite um numero de 1 a 5:"))
+
+console.log(raçaDeCachorro[raçaDeCachorro1])*/
+
+/*const raçaDeCachorro = ["PastorAlemão", "Husky", "Rottwailler", "Caramelo", "Shitsu"]
+const raçaDeCachorro1 = Number(prompt ("Digite um numero de 1 a 5:"))
+
+raçaDeCachorro[raçaDeCachorro1] === undefined ?
+console.log("Você deve informar números de 0 até 4"):
+console.log(raçaDeCachorro[raçaDeCachorro1]);*/
+
+/*const numero = ["a", "b", "c", "d", "e"]
+
+console.log(numero,length);
+console.log(numero.includes("a"));*/
+
+/*const numero = ["a", "b", "c", "d", "e"]
+numero.push(prompt("digite uma letra"))
+console.log(numero)*/
+
+/*const letras = ["a", "b", "c", "d", "e", "f", "g"]
+letras.splice(2,1)
+
+console.log(letras)*/
+
+/*const numero = [1, 2, 3, 4, 5, 6]
+console.log(numero.length);
+numero.push(7)
+numero.splice(3,2)
+console.log(numero.length);*/
